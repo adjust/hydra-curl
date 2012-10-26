83caf56e379b2a57d6dcdac3a0a1c42efeaef463
Paul H. Müller <paul@adeven.com>
2012-10-26 10:53:39 +0200

    use xargs, thx to Bob

--- hydra-curl.sh
+++ hydra-curl.sh
@@ -3,12 +3,8 @@ MAX_FORKS=100
 TODO_FILE=$1
 TARGET=$2
 DOWNLOAD_FOLDER="/tmp/hydra-curl"
-TIME_STAMP=$(date +%s)
-let "BLOCK_SIZE = MAX_FORKS/5"
-CURRENT_TODO=0
-COUNTER=0
 
-function download {
+function curl_download {
   ANSWER_SIZE=0
   MIN_SIZE=10000
   MAX_RETRIES=5
@@ -17,7 +13,7 @@ function download {
   TRIES=0
   while [ "$TRIES" -lt "$MAX_RETRIES" ] && [ "$ANSWER_SIZE" -lt "$MIN_SIZE" ]
     do
-      ANSWER=$(curl --retry 3 -s "$10$TRIES" --max-time 60)
+      ANSWER=$(curl --retry 3 -s "$2?$1$TRIES" --max-time 60)
       if [ "$?" -ne "0" ]; then
         ANSWER=""
       else
@@ -28,29 +24,15 @@ function download {
   
   if [ "$ANSWER_SIZE" -ge "$MIN_SIZE" ]; then
     ANSWER=$ANSWER"$DELIMITER"
-    echo $ANSWER > $2
+    echo $ANSWER > $3/$1
   fi
 }
 
+export -f curl_download
+
 mkdir $DOWNLOAD_FOLDER
 
-filecontent=( `cat $TODO_FILE `)
-
-for t in "${filecontent[@]}"
-do
-  if [ "$CURRENT_TODO" -le 0 ]; then
-    while [ $(ps aux | grep curl |wc -l) -ge "$MAX_FORKS" ]
-     do
-       sleep 0.5
-     done
-    let "CURRENT_TODO += BLOCK_SIZE"
-  fi
-  download "$t?$TIME_STAMP" "$DOWNLOAD_FOLDER/$COUNTER" &
-  let "COUNTER += 1"    
-  let "CURRENT_TODO -= 1"
-done
-
-wait
+cat -n $TODO_FILE | xargs -P $MAX_FORKS -n 3 -I{} bash -c curl_download\ \{\}\ $DOWNLOAD_FOLDER
 
 cat $DOWNLOAD_FOLDER/* > $TARGET
 rm -rf $DOWNLOAD_FOLDER
